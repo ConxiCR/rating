bd7b594b2a7c69a19f481649ba8011b5a6dc8bfc
ConxiCR <conxicr@hotmail.com>
2021-01-25 16:43:33 +0100

    modificacions en main.j per remove de color

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-let ratingStar = document.getElementById('ratingStar');
 
-//agafo qualsevol click dintre del lloc "e" nom de l'argument
-ratingStar.addEventListener("click", function(e){
-  let radio = e.target.id;
-  let id_select = radio.replace('radio', ' ');
-  stars(id_select);
+window.addEventListener("click", function (event) {
+    //capturo cualquier click en cualquier sitio "e" es el nombre argument
+    if (!document.getElementById("ratingStar").contains(event.target)) {
+      //si el click NO está dentro del div, limpiar todo
+    }cleanColor();
+  });
 
-}, false);
-
-function stars(parameter){
- 
-  for (let i = 0; i < parameter.length; i++){
-    cleanColor[i].classList.remove("activeStar");
-  }
-}
 function cleanColor(){
-  for (let i = 0; i >= 5; i++){
-    document.getElementById("rating" +i).classList.remove("checked");
-    document.getElementById("rating" +i).classList.add("unchecked");
-  }
+    document.getElementById("ratingStar").classList.remove("rgb(255,165,0)");
+    document.getElementById("ratingStar").classList.add("white)");
 }
-
+   
